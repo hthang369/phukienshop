a7eedc9f7154688db96411d34df9b7ed4af291ec
[Lampart] Tran Hoang Thang <hoang_thang@lampart-vn.com>
2022-02-18 15:09:54 +0700

    update

---
+++ resources/lang/vi/common.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'contacts' => [
+        'subject' => 'Chủ đề liên hệ',
+        'fullname' => 'Họ tên',
+        'email' => 'Email',
+        'phone' => 'Số điện thoại',
+        'address' => 'Địa chỉ',
+        'gender' => 'Giới tính',
+        'content' => 'Nội dung'
+    ],
+    'sidebar' => [
+        'support' => 'Hỗ trợ online',
+        'service' => 'Dịch vụ',
+        'products' => 'Sản phẩm',
+        'typical_products' => 'Sản phẩm tiêu biểu'
+    ],
+    'counter' => [
+        'title' => 'Lượt truy cập',
+        'isOnline' => 'Đang truy cập',
+        'todayOnline' => 'Truy cập hôm nay',
+        'totalOnline' => 'Tổng số truy cập'
+    ]
+];
